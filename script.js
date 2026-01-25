d9c0d41cabd25bc01588936b05c6f7751404b16e
jmjunior20 <jmjuniormonteiro@gmail.com>
2026-01-24 22:53:22 -0300

    Resultado jogo 1 rodada 6

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ const rodadas = [
     {
         nome: "6ª Rodada",
         jogos: [
-            {a:times[1], b:times[2], ga:null, gb:null, video: ""},
+            {a:times[1], b:times[2], ga:5, gb:6, video: ""},
             {a:times[4], b:times[5], ga:null, gb:null, video: ""},
             {a:times[0], b:times[3], ga:null, gb:null, video: ""}
         ]
